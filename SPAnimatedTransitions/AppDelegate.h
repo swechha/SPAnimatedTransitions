79f20482458f6b55ba9981f4dbea21cce7e10321
Swechha Prakash <swechhaprakash@gmail.com>
2014-04-16 20:47:54 +0530

    Initial Commit

---
+++ SPAnimatedTransitions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPAnimatedTransitions
+//
+//  Created by Swechha Prakash on 16/04/14.
+//  Copyright (c) 2014 Swechha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPAnimatedTransitionsTests/SPAnimatedTransitionsTests.m
@@ -0,0 +1,34 @@
+//
+//  SPAnimatedTransitionsTests.m
+//  SPAnimatedTransitionsTests
+//
+//  Created by Swechha Prakash on 16/04/14.
+//  Copyright (c) 2014 Swechha. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SPAnimatedTransitionsTests : XCTestCase
+
+@end
+
+@implementation SPAnimatedTransitionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
